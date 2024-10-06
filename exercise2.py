9733dd0e072838271edd691bef55c77e1e24ffcc
edmharx <edmharxx@gmail.com>
2024-10-06 22:30:50 +0800

    tuples

--- exercise2.py
+++ exercise2.py
@@ -1,3 +1,6 @@
 list1 = ["Food","Car","Movie","Book"]
 list2 = ["Adobo", "Subaru", "Transformers", "Sherlock Holmes"]
 
+listOfTuple = [(list1[x], list2[x])for x in range(len(list1))]
+
+print (listOfTuple)
